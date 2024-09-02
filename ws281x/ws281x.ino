d01d7f630ec8cbc03308a996c53df090cd807ecf
xuzhenxing <xrnmail@163.com>
2024-09-01 23:05:01 -0700

    Optimize ws281x.ino codes

--- ws281x/ws281x.ino
+++ ws281x/ws281x.ino
@@ -3,20 +3,24 @@
 #include <Adafruit_NeoPixel.h>
 
 #define WS281X_PIN	4
+#define PIXEL_NUM   20
 
 // Refer to https://adafruit.github.io/Adafruit_NeoPixel/html/class_adafruit___neo_pixel.html
-// NEO_KHZ800: WS2812, NEO_KHZ400: WS2811
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, WS281X_PIN, NEO_GRB + NEO_KHZ800);
+// NEO_KHZ800: WS2811/WS2812/WS2812B
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_NUM, WS281X_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin();
   strip.show();
 }
 
+// Loop for red, green, blue, white and black
 void loop() {
-  colorWipe(strip.Color(255, 0, 0), 1000);
+  colorWipe(strip.Color(255, 0, 0), 3000);
   colorWipe(strip.Color(0, 255, 0), 3000);
-  colorWipe(strip.Color(0, 0, 255), 6000);
+  colorWipe(strip.Color(0, 0, 255), 3000);
+  colorWipe(strip.Color(255, 255, 255), 3000);
+  colorWipe(strip.Color(0, 0, 0), 3000);
 }
 
 void colorWipe(uint32_t c, uint16_t wait) {
